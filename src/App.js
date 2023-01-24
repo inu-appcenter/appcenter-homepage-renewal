cc82f3cc0b8d85e19022daaf6ea2d7b5e219f314
milk717 <robolindasoo@gmail.com>
2023-01-24 17:25:50 +0900

    feat: 기본 경로 /home 으로 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import HomePage from "./page/HomePage";
 import FAQMainPage from "./page/FAQMainPage";
 import FAQDetailPage from "./page/FAQDetailPage";
@@ -10,17 +10,13 @@ import MainPage from "./page/MainPage";
 function App() {
     return (
         <Routes>
-            <Route path='' element={<MainPage/>}>
+            <Route path='/' element={<Navigate to={'home'}/>}/>
+            <Route path='/' element={<MainPage/>}>
                 <Route path={routerPath.home.url} element={<HomePage/>}/>
                 <Route path={routerPath.join.url} element={<JoinUsPage/>}/>
                 <Route path={routerPath.faq.url} element={<FAQMainPage/>}/>
                 <Route path={routerPath.faqDetail.url} element={<FAQDetailPage/>}>
-                    <Route path={routerPath.faqDetail.child.common.url} element={<FAQDetailListContainer/>}/>
-                    <Route path={routerPath.faqDetail.child.android.url} element={<FAQDetailListContainer/>}/>
-                    <Route path={routerPath.faqDetail.child.ios.url} element={<FAQDetailListContainer/>}/>
-                    <Route path={routerPath.faqDetail.child.server.url} element={<FAQDetailListContainer/>}/>
-                    <Route path={routerPath.faqDetail.child.web.url} element={<FAQDetailListContainer/>}/>
-                    <Route path={routerPath.faqDetail.child.design.url} element={<FAQDetailListContainer/>}/>
+                    <Route path=':part' element={<FAQDetailListContainer/>}/>
                 </Route>
             </Route>
         </Routes>
