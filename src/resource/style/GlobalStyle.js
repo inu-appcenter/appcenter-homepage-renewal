c6d50e88adba68ee3661dd8007f9cd7d4ba325b2
milk717 <robolindasoo@gmail.com>
2023-02-03 12:47:03 +0900

    design: Pretandard 전역 폰트 설정

--- src/resource/style/GlobalStyle.js
+++ src/resource/style/GlobalStyle.js
@@ -1,26 +1,54 @@
 import {createGlobalStyle} from "styled-components";
-// import {viewHeightCalc} from "../../lib/viewportCalculate";
+import PretendardMedium from '../fonts/Pretendard-Medium.otf'
+import PretendardSemiBold from '../fonts/Pretendard-SemiBold.otf'
+import PretendardBold from '../fonts/Pretendard-Bold.otf'
 
 const GlobalStyle = createGlobalStyle`
   //이 안에 전체 프로젝트에 적용될 css를 작성하면 됩니다~!
+  *{
+    font-family: 'Pretendard', sans-serif;
+  }
+  
   body {
     margin: 0;
   }
 
-  nav{
-    position:sticky;
-    top:0;
+  nav {
+    position: sticky;
+    top: 0;
   }
-  
-  main{
+
+  main {
     width: 70%;
     margin-left: auto;
     margin-right: auto;
   }
-  
+
   a {
     text-decoration: none;
   }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url(${PretendardMedium});
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url(${PretendardSemiBold});
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url(${PretendardBold});
+    font-weight: 700;
+    font-style: normal;
+  }
+  
 `
 
 export default GlobalStyle;
