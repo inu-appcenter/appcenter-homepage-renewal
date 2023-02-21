aa3a759843755efaabbc0f13282e7a1d6e5a8469
milk717 <robolindasoo@gmail.com>
2023-02-21 16:15:54 +0900

    feat: ourTeam 링크 연결

--- src/component/ourteam/MemberItem.js
+++ src/component/ourteam/MemberItem.js
@@ -2,12 +2,11 @@ import Card from "@mui/joy/Card";
 import Box from "@mui/joy/Box";
 import AspectRatio from "@mui/joy/AspectRatio";
 import CardCover from "@mui/joy/CardCover";
-import IconButton from "@mui/joy/IconButton";
-import CreateNewFolder from "@mui/icons-material/CreateNewFolder";
-import Favorite from "@mui/icons-material/Favorite";
 import Typography from "@mui/joy/Typography";
 import * as React from "react";
 import styled from "styled-components";
+import {linkToIcon,hostNameFirstWord} from "../../lib/linkToIcon";
+import {Avatar} from "@mui/joy";
 
 export default function MemberItem({image, name, description, link}) {
     return (
@@ -24,7 +23,7 @@ export default function MemberItem({image, name, description, link}) {
                         <img
                             src={image}
                             loading="lazy"
-                            alt="Yosemite by Casey Horner"
+                            alt={`${name}의 사진`}
                         />
                     </figure>
                 </AspectRatio>
@@ -40,25 +39,6 @@ export default function MemberItem({image, name, description, link}) {
                             'linear-gradient(180deg, transparent 32%, rgba(0,0,0,0.00345888) 63.94%, rgba(0,0,0,0.014204) 65.89%, rgba(0,0,0,0.0326639) 67.83%, rgba(0,0,0,0.0589645) 69.78%, rgba(0,0,0,0.0927099) 71.72%, rgba(0,0,0,0.132754) 73.67%, rgba(0,0,0,0.177076) 75.61%, rgba(0,0,0,0.222924) 77.56%, rgba(0,0,0,0.267246) 79.5%, rgba(0,0,0,0.30729) 81.44%, rgba(0,0,0,0.341035) 83.39%, rgba(0,0,0,0.367336) 85.33%, rgba(0,0,0,0.385796) 87.28%, rgba(0,0,0,0.396541) 89.22%, rgba(0,0,0,0.4) 91.17%)',
                     }}
                 >
-                    <Box>
-                        <Box
-                            sx={{
-                                p: 2,
-                                display: 'flex',
-                                alignItems: 'center',
-                                gap: 1.5,
-                                flexGrow: 1,
-                                alignSelf: 'flex-end',
-                            }}
-                        >
-                            <IconButton size="sm" color="neutral" sx={{ml: 'auto'}}>
-                                <CreateNewFolder/>
-                            </IconButton>
-                            <IconButton size="sm" color="neutral">
-                                <Favorite/>
-                            </IconButton>
-                        </Box>
-                    </Box>
                 </CardCover>
             </Box>
             <Box sx={{display: 'flex', gap: 1, mt: 1.5, alignItems: 'center'}}>
@@ -70,6 +50,19 @@ export default function MemberItem({image, name, description, link}) {
                 <Typography sx={{fontSize: '20px', fontWeight: 'md'}}>
                     {description}
                 </Typography>
+            </Box>
+            <Box sx={{display: 'flex', gap: 1, mt: 1.5, alignItems: 'center'}}>
+                {
+                    link.map(v=>(
+                        <Avatar
+                            onClick={()=>{window.open(v)}}
+                            sx={{ bgcolor: `${linkToIcon(v) || '#DCEAFA' }` }}
+                            src={linkToIcon(v)}
+                            alt={`${name}의 웹사이트`}>
+                            {hostNameFirstWord(v)}
+                        </Avatar>
+                    ))
+                }
             </Box>
         </StyledCard>
     );
