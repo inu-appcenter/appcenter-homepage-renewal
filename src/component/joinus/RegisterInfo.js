fbccf84e30471da59e8ee8377c63e1987da5b0d0
milk717 <robolindasoo@gmail.com>
2023-02-28 14:09:07 +0900

    feat: Join Us 상단 반응형 수정

--- src/component/joinus/RegisterInfo.js
+++ src/component/joinus/RegisterInfo.js
@@ -1,52 +1,53 @@
 import styled from "styled-components";
-import {viewHeightCalc} from "../../lib/viewportCalculate";
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import {Button} from "@mui/material";
 
 export default function RegisterInfo() {
     const text = 'Ready To get\nstarted?'
     return (
-        <RegisterInfoWrapper>
-            <RegisterTextWrapper>
-                <div className='top'>app center</div>
-                <div className='middle'>{text}</div>
-                <Button variant='outlined'>
-                    지원 방법 바로가기
-                    <ArrowRightAltIcon sx={{ fontSize: 40 }}/>
-                </Button>
-            </RegisterTextWrapper>
-        </RegisterInfoWrapper>
+        <RegisterTextWrapper>
+            <div className='top'>app center</div>
+            <div className='middle'>{text}</div>
+            <Button variant='outlined'>
+                지원 방법 바로가기
+                <ArrowRightAltIcon sx={{fontSize: 40}}/>
+            </Button>
+        </RegisterTextWrapper>
     );
 }
 
-const RegisterInfoWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
 const RegisterTextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  justify-items: center;
+  row-gap: 16px;
   color: ${props => props.theme.color.yellow};
+  padding: 12px 0;
 
   .top {
     font-size: 14px;
     font-weight: 600;
-    margin-bottom: ${viewHeightCalc(24)};
     text-transform: uppercase;
   }
 
   .middle {
-    font-size: 64px;
     font-weight: 500;
     white-space: pre-line;
     text-align: center;
-    margin-bottom: ${viewHeightCalc(58)}
+    font-size: ${props => props.theme.fontSize.bigDesktop.title};
+    @media (max-width: 1800px) {
+      font-size: ${props => props.theme.fontSize.desktop.title};
+    }
+    @media (max-width: 1200px) {
+      font-size: ${props => props.theme.fontSize.tablet.title};
+    }
+    @media (max-width: 768px) {
+      font-size: ${props => props.theme.fontSize.smallTablet.title};
+    }
+    @media (max-width: 576px) {
+      font-size: ${props => props.theme.fontSize.mobile.title};
+    }
   }
-;
+
 }
 
 & > Button {
@@ -54,7 +55,26 @@ const RegisterTextWrapper = styled.div`
   background: ${props => props.theme.color.white};
   border: 1px solid ${props => props.theme.color.yellow};
   color: ${props => props.theme.color.yellow};
-  font-size: 18px;
+  padding: 0 8px;
+
+  &:hover {
+    background: rgba(254, 200, 83, 0.1);
+    border: 1px solid ${props => props.theme.color.yellow};
+  }
+
+  font-size: ${props => props.theme.fontSize.bigDesktop.text};
+  @media (max-width: 1800px) {
+    font-size: ${props => props.theme.fontSize.desktop.text};
+  }
+  @media (max-width: 1200px) {
+    font-size: ${props => props.theme.fontSize.tablet.text};
+  }
+  @media (max-width: 768px) {
+    font-size: ${props => props.theme.fontSize.smallTablet.text};
+  }
+  @media (max-width: 576px) {
+    font-size: ${props => props.theme.fontSize.mobile.text};
+  }
 }
 `
 
--- src/page/JoinUsPage.js
+++ src/page/JoinUsPage.js
@@ -2,14 +2,32 @@ import JoinUsMainContainer from "../container/joinjs/JoinUsMainContainer";
 import ArchiveContainer from "../container/joinjs/ArchiveContainer";
 import RegisterStepContainer from "../container/joinjs/RegisterStepContainer";
 import ContactContainer from "../container/joinjs/ContactContainer";
+import styled from "styled-components";
+import {PageTitle} from "../component/common/PageTitle";
 
 export default function JoinUsPage(){
     return(
-        <>
+        <JoinUsPageWrapper>
+            <TitleWrapper>
+                <PageTitle
+                    title="Join Us!"
+                />
+            </TitleWrapper>
             <JoinUsMainContainer/>
             <ArchiveContainer/>
             <RegisterStepContainer/>
             <ContactContainer />
-        </>
+        </JoinUsPageWrapper>
     );
 }
+const JoinUsPageWrapper = styled.div`
+  padding: 0 2rem;
+  display: grid;
+  grid-template-columns: 100%;
+  row-gap: 2rem;
+`
+
+const TitleWrapper = styled.div`
+  padding-top: ${props=>props.theme.padding.bigDesktop.contentTop};
+
+`
