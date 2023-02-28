0c8b94fee6b3e852a1eb8d8e68a682f5ca0d8725
milk717 <robolindasoo@gmail.com>
2023-02-28 16:16:26 +0900

    design: homepage about us 반응형 대응

--- src/container/home/ProductContainer.js
+++ src/container/home/ProductContainer.js
@@ -11,7 +11,7 @@ export default function ProductContainer(){
                 topMargin={viewHeightCalc(50)}
                 subTitle='앱센터에서 만든 앱들을 소개합니다'
             />
-            <TempImage src={TempAppImage} alt='임시 이미지'/>
+            {/*<TempImage src={TempAppImage} alt='임시 이미지'/>*/}
         </ProductWrapper>
     );
 }
