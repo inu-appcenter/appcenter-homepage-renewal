3dad10c9b4592d44ec93497262a1df946ed2e878
milk717 <robolindasoo@gmail.com>
2023-03-01 22:02:45 +0900

    design: 슬로건 그림자 제거

--- src/component/aboutus/Slogan.js
+++ src/component/aboutus/Slogan.js
@@ -21,7 +21,6 @@ const Text = styled.div`
   text-align: center;
   letter-spacing: 1px;
   text-transform: uppercase;
-  text-shadow: ${props => props.type === 'top' && `0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)`};
   color: ${props => props.type === 'title' ? '#FEC853' : '#1E4995'};
   font-weight: ${props => props.type === 'top' ? 600 : props.type === 'title' ? 700 : 400};
   margin-bottom: ${viewWidthCalc(24)};
