dbaa73222b8c64284f640b9e9b71ea4bd469ef80
YunDH218 <68414997+YunDH218@users.noreply.github.com>
2023-03-04 22:00:15 +0900

    chore: 페이지 이동 시 스크롤 0로 초기화

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,8 +1,14 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import HeaderSwitcher from "../container/home/HeaderSwitcher";
 import Footer from "../component/common/Footer";
+import {useEffect} from "react";
 
 export default function MainPage() {
+    const { pathname } = useLocation();
+    
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
     return (
         <>
             <div>
