17c15c6e4b97d7bc6dc13b8df1b7b09392acc0e6
milk717 <robolindasoo@gmail.com>
2023-03-05 11:57:13 +0900

    design: 홈화면 간격 조정

--- src/component/home/Slogan.js
+++ src/component/home/Slogan.js
@@ -31,7 +31,7 @@ export default function Slogan() {
 }
 
 const SloganWrapper = styled.div`
-  margin-top: 80px;
+    
 `;
 
 const Text = styled.div`
@@ -43,6 +43,7 @@ const Text = styled.div`
   font-weight: ${props => props.type === 'top' ? 600 : props.type === 'title' ? 700 : 400};
   margin-bottom: ${viewWidthCalc(24)};
   white-space: pre-line;
+  word-break: keep-all;
   ${props => props.type === 'title' ?
           css`font-size: ${props => props.theme.fontSize.bigDesktop.title};
             @media (max-width: 1800px) {
