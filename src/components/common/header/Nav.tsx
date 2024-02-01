b50f0c7fd834366efb3cb8e2aa86cd2c8a6dc634
Sumin <robolindasoo@gmail.com>
2024-02-01 14:03:19 +0900

    feat: 하위메뉴 nav 추가

--- src/components/common/header/Nav.tsx
+++ src/components/common/header/Nav.tsx
@@ -1,9 +1,38 @@
-const Nav = () => {
-    return(
-        <ul className="bg-primary-700">
+import { MENU } from '../../../constants/menu.ts';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { PartParam } from '../../../types/common.ts';
 
-        </ul>
-    )
-}
+const Nav = () => {
+  const { pathname, hash } = useLocation();
+  const { part } = useParams<PartParam>();
+
+  const isChildrenActive = (path: string) => {
+    return pathname.replace('/' + part ?? '', '') === path;
+  };
+  return (
+    <ul className='bg-primary-700'>
+      {MENU.map(({ path, label, children }) => (
+        <li
+          key={path}
+          className={`${isChildrenActive(path) || pathname === path ? 'text-secondary-300' : 'text-white'} font-semibold`}
+        >
+          <Link to={path}>{label}</Link>
+          {children && (
+            <ul>
+              {children.map(({ path, label }) => (
+                <li
+                  key={path}
+                  className={`${pathname + hash === path ? 'text-secondary-300' : 'text-white'} font-semibold`}
+                >
+                  <Link to={path}>{label}</Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Nav;
