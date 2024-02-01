5e6494ed9c8d7cec7500817ecd6ff4e1daae20dc
Sumin <robolindasoo@gmail.com>
2024-02-01 12:13:00 +0900

    feat: team 파라미터 타입 추가

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,6 +1,11 @@
+import {useParams} from "react-router-dom";
+import {PartParam} from "../types/common.ts";
+
 const TeamPage = () => {
+   const {part} = useParams<PartParam>();
+
    return(
-       <div>Our Team</div>
+       <div>Our Team {part} </div>
    )
 }
 
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,4 @@
 import {PART} from "../constants/common.ts";
 
 export type Part = keyof typeof PART;
+export type PartParam = { part: Part;}
