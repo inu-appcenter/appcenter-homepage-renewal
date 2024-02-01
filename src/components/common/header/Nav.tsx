04c2e4bd54f1eb005bfebf9ca60f8ca60e7fc6f0
Sumin <robolindasoo@gmail.com>
2024-02-01 23:04:39 +0900

    design: 하위 메뉴에 투명도 애니메이션 추가

--- src/components/common/header/Nav.tsx
+++ src/components/common/header/Nav.tsx
@@ -16,18 +16,16 @@ const Nav = () => {
           className={`relative group ${isChildrenActive(path) || pathname === path ? 'text-secondary-300' : 'text-white'} text-xl font-semibold`}
         >
           <Link to={path}>{label}</Link>
-          {children && (
-            <ul className='absolute top-full left-1/2 transform -translate-x-1/2 p-4 rounded-2xl bg-primary-700 hidden group-hover:flex flex-col gap-y-3 flex-1'>
-              {children.map(({ path, label }) => (
-                <li
-                  key={path}
-                  className={`text-center w-24 ${pathname + hash === path ? 'text-secondary-300' : 'text-white'} font-semibold`}
-                >
-                  <Link to={path}>{label}</Link>
-                </li>
-              ))}
-            </ul>
-          )}
+          <ul className='absolute top-full left-1/2 transform -translate-x-1/2 p-4 flex flex-col gap-y-3 rounded-2xl bg-primary-700 opacity-0 group-hover:opacity-100 transition-opacity duration-150'>
+            {children?.map(({ path, label }) => (
+              <li
+                key={path}
+                className={`text-center w-24 ${pathname + hash === path ? 'text-secondary-300' : 'text-white'} font-semibold`}
+              >
+                <Link to={path}>{label}</Link>
+              </li>
+            ))}
+          </ul>
         </li>
       ))}
     </ul>
