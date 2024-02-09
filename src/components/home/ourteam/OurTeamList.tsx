21d6adbcdf5ede88e0f8b32cd0543f44eb0df8e1
Sumin <robolindasoo@gmail.com>
2024-02-09 12:48:29 +0900

    feat: Our team 리스트 추가

--- src/components/home/ourteam/OurTeamList.tsx
+++ src/components/home/ourteam/OurTeamList.tsx
@@ -1,54 +1,57 @@
-const partInfo = [
+import { PATH } from '../../../constants/path.ts';
+import { PART } from '../../../constants/common.ts';
+import { Link } from 'react-router-dom';
+import ArrowRight from '../../../assets/svg/arrow_right.svg';
+
+const ourTeamList = [
   {
-    id: 0,
-    partName: 'Common',
-    question: '앱센터는 어떻게 들어오나요?',
-    description: '다른 분야가 모여 하나의 목표를 향해 나아갑니다',
-    speechBubble:
-      '이곳은 앱센터에 관련된 질문이 있는 공간입니다!\n' +
-      '공통질문과 각 파트별 질문이 나눠져 있으니\n' +
-      '더보기를 눌러 확인해보세요!',
-  },
-  {
-    id: 1,
-    partName: 'Android',
-    question: 'Java 와 Kotlin 중 어떤 언어를 쓰나요?',
+    label: PART.android.label,
     description: '안드로이드 운영체제에서 작동하는 앱을 구현합니다',
-    speechBubble: '안녕하세요! 안드로이드 파트 입니다.',
+    path: PATH.TEAM(PART.android.value),
   },
   {
-    id: 5,
-    partName: 'Design',
-    question: 'Figma, XD 할 줄 알아야 하나요?',
+    label: PART.design.label,
     description: '앱을 디자인하고 서비스를 설계합니다',
-    speechBubble:
-      '안녕하세요! 아이디어가 넘치는 디자인 입니다!\n함께 배우며 함께 성장하는 디자인팀으로 오세요!',
+    path: PATH.TEAM(PART.design.value),
   },
   {
-    id: 2,
-    partName: 'iOS',
-    question: '맥북 필수인가요?',
-    description: 'IOS 운영체제에서 작동하는 앱을 구현합니다',
-    speechBubble: '안녕하세요 ios 파트 입니다.',
+    label: PART.ios.label,
+    description: 'iOS 운영체제에서 작동하는 앱을 구현합니다',
+    path: PATH.TEAM(PART.ios.value),
   },
   {
-    id: 3,
-    partName: 'Server',
-    question: 'Node, Spring중 어떤 것을 사용하나요?',
+    label: PART.server.label,
     description: '서버를 구현합니다.',
-    speechBubble: '안녕하세요 서버 파트 입니다.\n',
+    path: PATH.TEAM(PART.server.value),
   },
   {
-    id: 4,
-    partName: 'Web',
-    question: 'React 할 줄 알아야 하나요?',
+    label: PART.web.label,
     description: '웹환경에 맞춘 서비스를 구현합니다',
-    speechBubble: '안녕하세요 웹 파트 입니다.\n',
+    path: PATH.TEAM(PART.web.value),
   },
 ];
 
 const OurTeamList = () => {
-  return <></>;
+  return (
+    <div className='flex mt-12 flex-col gap-8 h-4/5'>
+      {ourTeamList.map(({ label, description, path }) => (
+        <div className='flex justify-between items-center'>
+          <div>
+            <h4 className='text-4xl font-semibold mb-2 first-letter:text-primary-700'>
+              {label}
+            </h4>
+            <p className='text-xl font-medium text-gray-500'>{description}</p>
+          </div>
+          <Link
+            to={path}
+            className='flex items-center justify-center p-3 bg-amber-300 rounded-full w-12 h-12'
+          >
+            <img src={ArrowRight} />
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default OurTeamList;
--- src/components/home/ourteam/OurTeamSection.tsx
+++ src/components/home/ourteam/OurTeamSection.tsx
@@ -4,7 +4,7 @@ import OurTeamList from './OurTeamList.tsx';
 
 const OurTeamSection = () => {
   return (
-    <SectionLayout>
+    <SectionLayout className='justify-between'>
       <Title
         title='OUR TEAM'
         subtitle='다른 분야가 모여 하나의 목표를 향해 나아갑니다.'
