8c25fbf4dacd1e6d2b6ebed40a41f9fd92b92c05
Sumin <robolindasoo@gmail.com>
2024-02-12 22:13:11 +0900

    refactor: ourTeamList 외부로 분리

--- src/components/home/ourteam/OurTeamList.tsx
+++ src/components/home/ourteam/OurTeamList.tsx
@@ -1,36 +1,7 @@
 import { Link } from 'react-router-dom';
 import ArrowRight from '@assets/svg/arrow_right.svg';
-import { PART } from '@constants/common.ts';
-import { PATH } from '@constants/path.ts';
 import FirstLetterPointText from '@components/common/FirstLetterPointText.tsx';
-
-const ourTeamList = [
-  {
-    label: PART.android.label,
-    description: '안드로이드 운영체제에서 작동하는 앱을 구현합니다',
-    path: PATH.TEAM(PART.android.value),
-  },
-  {
-    label: PART.design.label,
-    description: '앱을 디자인하고 서비스를 설계합니다',
-    path: PATH.TEAM(PART.design.value),
-  },
-  {
-    label: PART.ios.label,
-    description: 'iOS 운영체제에서 작동하는 앱을 구현합니다',
-    path: PATH.TEAM(PART.ios.value),
-  },
-  {
-    label: PART.server.label,
-    description: '서버를 구현합니다.',
-    path: PATH.TEAM(PART.server.value),
-  },
-  {
-    label: PART.web.label,
-    description: '웹환경에 맞춘 서비스를 구현합니다',
-    path: PATH.TEAM(PART.web.value),
-  },
-];
+import { ourTeamList } from '@assets/text/team.ts';
 
 const OurTeamList = () => {
   return (
