8e048d982f405c251b10be45d16f043cd2a27a9a
Sumin <robolindasoo@gmail.com>
2024-02-12 22:25:38 +0900

    refactor: TeamSubTitle 컴포넌트 분리

---
+++ src/components/team/TeamSubTitle.tsx
@@ -0,0 +1,25 @@
+import FirstLetterPointText from '@components/common/FirstLetterPointText.tsx';
+import { useParams } from 'react-router-dom';
+import { PartParam } from '@type/common.ts';
+import { useMemo } from 'react';
+import { ourTeamList } from '@assets/text/team.ts';
+import { PART } from '@constants/common.ts';
+
+const TeamSubTitle = () => {
+  const { part } = useParams<PartParam>();
+
+  const { label, description } = useMemo(
+    () =>
+      ourTeamList.find(({ label }) => label === PART[part ?? 'android'].label)!,
+    [part]
+  );
+
+  return (
+    <div className='flex items-center flex-wrap gap-x-8'>
+      <FirstLetterPointText text={label} />
+      <p className='text-grayscale-500 font-semibold text-lg'>{description}</p>
+    </div>
+  );
+};
+
+export default TeamSubTitle;
--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,32 +1,15 @@
-import { Suspense, useMemo } from 'react';
+import { Suspense } from 'react';
 import TeamMemberList from '@components/team/TeamMemberList.tsx';
 import Title from '@components/common/Title.tsx';
-import { useParams } from 'react-router-dom';
-import { PartParam } from '@type/common.ts';
-import FirstLetterPointText from '@components/common/FirstLetterPointText.tsx';
-import { PART } from '@constants/common.ts';
-import { ourTeamList } from '@assets/text/team.ts';
+import TeamSubTitle from '@components/team/TeamSubTitle.tsx';
 
 const TeamPage = () => {
-  const { part } = useParams<PartParam>();
-
-  const { label, description } = useMemo(
-    () =>
-      ourTeamList.find(({ label }) => label === PART[part ?? 'android'].label)!,
-    [part]
-  );
-
   return (
     <div className='p-8'>
       <div className='mb-8'>
         <Title title='OUR TEAM' />
       </div>
-      <div className='flex items-center flex-wrap gap-x-8'>
-        <FirstLetterPointText text={label} />
-        <p className='text-grayscale-500 font-semibold text-lg'>
-          {description}
-        </p>
-      </div>
+      <TeamSubTitle />
       <Suspense>
         <TeamMemberList />
       </Suspense>
