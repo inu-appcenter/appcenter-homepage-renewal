d0895791e3bd2178fba6471ce36efdf8b3c6dc76
Sumin <robolindasoo@gmail.com>
2024-02-12 20:28:58 +0900

    feat: tailwind 테마와 chakra 테마 연동

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRouter from '@router/AppRouter.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import chakraTheme from '@/chakraTheme.ts';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ChakraProvider>
+        <ChakraProvider theme={chakraTheme}>
           <AppRouter />
         </ChakraProvider>
       </QueryClientProvider>
