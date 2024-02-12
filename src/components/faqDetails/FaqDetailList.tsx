93d82b820a0423442ac6efd0b315bcd983033b34
Sumin <robolindasoo@gmail.com>
2024-02-12 18:15:37 +0900

    feat: faq 페이지에 아코디언 적용

--- src/components/faqDetails/FaqDetailList.tsx
+++ src/components/faqDetails/FaqDetailList.tsx
@@ -1,25 +1,44 @@
 import { useParams } from 'react-router-dom';
 import { PartParam } from '@type/common.ts';
 import useGetFaqsPublicAllQuery from '@api/query/useGetFaqsPublicAllQuery.ts';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+} from '@chakra-ui/react';
 
 const FaqDetailList = () => {
   const { part } = useParams<PartParam>();
   const { data } = useGetFaqsPublicAllQuery(part ?? 'android');
 
   return (
-    <ul className='flex flex-col gap-4'>
-      {data.map(({ id, question, answer }) => (
-        <li
-          key={id}
-          className='flex flex-col gap-y-2 p-4 bg-primary-200 rounded-3xl'
-        >
-          <p className='relative py-2 px-4 bg-white rounded-3xl font-semibold before:content-["Q."] before:mr-1 before:leading-4 before:font-semibold'>
-            {question}
-          </p>
-          <p className='px-4 text-primary-800 font-medium'>{answer}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <Accordion className='flex flex-col gap-4'>
+        {data.map(({ id, question, answer }) => (
+          <AccordionItem key={id} className='p-4 bg-primary-200 rounded-3xl'>
+            <h2 className='rounded-3xl bg-white'>
+              <AccordionButton className='rounded-3xl'>
+                <Box
+                  as='span'
+                  flex='1'
+                  textAlign='left'
+                  className='font-semibold before:content-["Q."] before:mr-1 before:leading-4 before:font-semibold'
+                >
+                  {question}
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={0} className='text-primary-800 font-medium'>
+              {answer}
+            </AccordionPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
   );
 };
 
