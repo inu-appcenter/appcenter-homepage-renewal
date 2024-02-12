1b767c397d279a225ab7240521edb38b3032ae6a
Sumin <robolindasoo@gmail.com>
2024-02-12 21:27:48 +0900

    feat: faq 로딩 스피너 추가

--- src/pages/FAQDetailPage.tsx
+++ src/pages/FAQDetailPage.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { PartParam } from '@type/common.ts';
 import { PART } from '@constants/common.ts';
 import FaqDetailTabs from '@components/faqDetails/FaqDetailTabs.tsx';
+import { Spinner } from '@chakra-ui/react';
 
 const FAQDetailPage = () => {
   const { part } = useParams<PartParam>();
@@ -13,7 +14,13 @@ const FAQDetailPage = () => {
     <div className='flex flex-col gap-y-8 p-8 mx-auto max-w-screen-md'>
       <Title title={`${PART[part ?? 'android'].label} FAQ`} />
       <FaqDetailTabs />
-      <Suspense>
+      <Suspense
+        fallback={
+          <div className='flex justify-center items-center h-52'>
+            <Spinner />
+          </div>
+        }
+      >
         <FaqDetailList />
       </Suspense>
     </div>
