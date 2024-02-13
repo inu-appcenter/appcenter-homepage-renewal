399038fb75861db7d45093a1da0b11252843cd4a
Sumin <robolindasoo@gmail.com>
2024-02-13 11:48:10 +0900

    design: 말풍선 꼬리 모양 수정

--- src/components/common/SpeechBubble.tsx
+++ src/components/common/SpeechBubble.tsx
@@ -7,7 +7,7 @@ const SpeechBubble: React.FC<SpeechBubbleProps> = ({ children, className }) => {
     <p
       className={
         className +
-        ' relative bg-secondary-100 p-6 pl-4 rounded-3xl border-l-8 lg:border-l-[17px] border-secondary-100 h-fit before:absolute before:-left-8 before:bottom-0 before:h-8 before:border-[32px] before:rounded-br-6xl before:rounded-bl-lg before:border-secondary-100 before:-z-10 after:absolute after:-left-[4.5rem] after:bottom-0 after:h-8 after:border-[32px] after:rounded-br-4xl after:border-white after:-z-10'
+        ' relative bg-secondary-100 p-6 pl-4 rounded-2xl h-fit before:absolute before:-left-8 before:bottom-0 before:h-14 before:w-16 before:rounded-br-6xl before:rounded-bl-xl before:bg-secondary-100 before:-z-10 after:absolute after:left-0 after:bottom-0 after:-translate-x-full after:w-9 after:h-16 after:bg-white after:rounded-br-7xl'
       }
     >
       {children}
