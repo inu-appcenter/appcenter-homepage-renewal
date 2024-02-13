eb8e533671dc77fe0aeba46feab2129dc6628ca3
Sumin <robolindasoo@gmail.com>
2024-02-13 09:03:54 +0900

    fix: 타입 에러 수정

--- src/router/loader/teamPageLoader.ts
+++ src/router/loader/teamPageLoader.ts
@@ -1,10 +1,10 @@
 import { PART } from '@constants/common.ts';
-import { redirect } from 'react-router-dom';
+import { Params, redirect } from 'react-router-dom';
 import { PATH } from '@constants/path.ts';
 import { PartParam } from '@type/common.ts';
 
-const teamPageLoader = ({ params }: { params: PartParam }) => {
-  const { part } = params;
+const teamPageLoader = ({ params }: { params: Params }) => {
+  const { part } = params as PartParam;
   const teamList = Object.keys(PART).filter((part) => part !== 'common');
 
   if (!teamList.includes(part)) return redirect(PATH.HOME());
