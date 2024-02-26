d6c2ab4e1c959c999a8cae6dc7d485413fa684e8
Sumin <robolindasoo@gmail.com>
2024-02-26 15:15:48 +0900

    feat: 메인화면 product 이미지 추가

--- src/components/home/product/ProductListSwiper.tsx
+++ src/components/home/product/ProductListSwiper.tsx
@@ -13,16 +13,21 @@ const ProductListSwiper = () => {
         modules={[Autoplay]}
         loop
         autoplay={{
-          delay: 0,
+          delay: 1000,
           disableOnInteraction: false,
         }}
         speed={2000}
         slidesPerView={5}
+        spaceBetween={30}
       >
         {data.map(({ id, title, images, androidStoreLink, appleStoreLink }) => (
           <SwiperSlide key={id}>
             <div className='flex flex-col gap-y-3'>
-              <img className='rounded-2xl' alt={title} src={images['1']} />
+              <img
+                className='border border-solid rounded-2xl'
+                alt={title}
+                src={Object.values(images).at(0)}
+              />
               <div className='flex flex-col gap-y-1'>
                 {androidStoreLink && (
                   <img
