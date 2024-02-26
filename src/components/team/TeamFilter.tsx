c7164ed47d5040bc199c4ce13efac7cb6e28d003
Sumin <robolindasoo@gmail.com>
2024-02-26 15:03:11 +0900

    fix: 파트 이동시 년도 변하는 문제 해결

--- src/components/team/TeamFilter.tsx
+++ src/components/team/TeamFilter.tsx
@@ -1,6 +1,6 @@
 import { MENU } from '@constants/menu.ts';
 import { Tab, TabList, Tabs } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import TeamYearDropdown from '@components/team/TeamYearDropdown.tsx';
 import { Suspense } from 'react';
 
@@ -8,6 +8,9 @@ const OurTeamPartList =
   MENU.find(({ label }) => label === 'Our Team')?.children ?? [];
 
 const TeamFilter = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
   return (
     <div className='flex flex-col gap-2'>
       <div className='flex gap-4'>
@@ -19,7 +22,7 @@ const TeamFilter = () => {
         <Tabs variant='soft-rounded'>
           <TabList>
             {OurTeamPartList.map(({ label, path }) => (
-              <Link key={path} to={path}>
+              <Link key={path} to={`${path}?year=${queryParams.get('year')}`}>
                 <Tab>{label}</Tab>
               </Link>
             ))}
