0bc64125d9e113f19daabf386d8f6ba8a61a53c3
ekkk1126 <cabi0203@naver.com>
2024-11-14 02:52:59 +0900

    feat: 라우터 설정

--- admin-2024/src/App.tsx
+++ admin-2024/src/App.tsx
@@ -1,8 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/common/Header';
+import { PATH } from './constants/path';
+import ClubPage from './page/clubPage';
+import GenerationPage from './page/generationPage';
+import MainPage from './page/mainPage';
+import MemberPage from './page/memberPage';
+import PhotoPage from './page/photoPage';
+import ProductPage from './page/productPage';
+import QnAPage from './page/qnaPage';
+import RolePage from './page/rolePage';
+import SignInPage from './page/SignInPage';
+
 function App() {
   return (
-    <>
-      <div className='bg-red-500'>tailwind css</div>
-    </>
+    <BrowserRouter>
+      {location.pathname !== PATH.SIGN_IN && <Header />}
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path={PATH.SIGN_IN} element={<SignInPage />} />
+        <Route path={PATH.CLUB} element={<ClubPage />} />
+        <Route path={PATH.MEMBER} element={<MemberPage />} />
+        <Route path={PATH.GENGERATION} element={<GenerationPage />} />
+        <Route path={PATH.ROLE} element={<RolePage />} />
+        <Route path={PATH.PRODUCT} element={<ProductPage />} />
+        <Route path={PATH.PHOTO} element={<PhotoPage />} />
+        <Route path={PATH.QNA} element={<QnAPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
