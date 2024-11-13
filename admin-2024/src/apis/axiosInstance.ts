d0c275f52492217bf9c3f9603c70a5eeffdc76e6
ekkk1126 <cabi0203@naver.com>
2024-11-13 20:49:35 +0900

    feat: axiosInstance 설정

---
+++ admin-2024/src/apis/axiosInstance.ts
@@ -0,0 +1,31 @@
+import { BASE_URL } from '@/constants/api';
+import axios, { AxiosInstance } from 'axios';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  headers: {
+    Accept: '*/*',
+    'Content-Type': 'application/json',
+  },
+  responseType: 'json',
+});
+
+// 요청 인터셉터
+axiosInstance.interceptors.request.use();
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  function (response) {
+    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 데이터가 있는 작업 수행
+    return response;
+  },
+  async function (error) {
+    // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 오류가 있는 작업 수행
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
