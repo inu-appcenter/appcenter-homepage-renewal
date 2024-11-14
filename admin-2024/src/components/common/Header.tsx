3f2c3dc79db2dc15af5be3bdf3414dde352fec5b
ekkk1126 <cabi0203@naver.com>
2024-11-14 13:59:10 +0900

    feat: 헤더 로그아웃 기능 추가

--- admin-2024/src/components/common/Header.tsx
+++ admin-2024/src/components/common/Header.tsx
@@ -1,14 +1,26 @@
 import SignInLogo from '@/assets/Signin_logo.png';
+import { PATH } from '@/constants/path';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const logOut = () => {
+    window.sessionStorage.removeItem('token');
+    delete axios.defaults.headers.common['X-AUTH-TOKEN'];
+    navigate(PATH.SIGN_IN);
+  };
   return (
     <header className='mb-6 mt-6 flex flex-row items-center justify-between bg-white'>
       <Link to='/'>
         <img src={SignInLogo} alt='Sign In Logo' className='h-auto w-10' />
       </Link>
-      <LogoutIcon fontSize='large' />
+      <LogoutIcon
+        fontSize='large'
+        className='cursor-pointer'
+        onClick={logOut}
+      />
     </header>
   );
 };
