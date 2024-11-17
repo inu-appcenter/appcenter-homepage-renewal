5b2e286b658672c286062a1a5a4f5bc9ac9f5619
ekkk1126 <cabi0203@naver.com>
2024-11-17 20:23:44 +0900

    style: 모달 박스 다크 모드 제거

--- admin-2024/src/components/common/ModalBox.tsx
+++ admin-2024/src/components/common/ModalBox.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ModalBox = ({ children }: { children: React.ReactNode }) => (
   <ModalPortal>
     <div className='fixed inset-0 z-[100] flex items-center justify-center bg-black/50'>
-      <div className='flex w-full max-w-xl flex-col items-center justify-center gap-6 rounded-lg bg-white p-6 shadow-lg dark:bg-gray-800'>
+      <div className='flex w-full max-w-xl flex-col items-center justify-center gap-6 rounded-lg bg-white p-6 shadow-lg'>
         {children}
       </div>
     </div>
