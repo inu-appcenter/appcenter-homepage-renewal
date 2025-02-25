e7f0dac818fa6c7e7b6da213ba3a44609d0a0623
baebaej <korea024504@gmail.com>
2025-02-26 01:44:42 +0900

    fix: 컨택 오타 수정

--- portal/src/components/home/product/ProductListSwiper.tsx
+++ portal/src/components/home/product/ProductListSwiper.tsx
@@ -4,42 +4,41 @@ import useGetIntroductionBoardPublicAllQuery from '@api/query/useGetIntroduction
 import ProductItem from '@components/home/product/ProductItem.tsx';
 
 const ProductListSwiper = () => {
-  const { data } = useGetIntroductionBoardPublicAllQuery();
+    const { data } = useGetIntroductionBoardPublicAllQuery();
 
-  return (
-    <div>
-      <Swiper
-        modules={[Autoplay, Pagination]}
-        pagination
-        navigation
-        loop
-        autoplay={{
-          delay: 500,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          475: {
-            slidesPerView: 3,
-          },
-          640: {
-            slidesPerView: 4,
-          },
-          1024: {
-            slidesPerView: 5,
-          },
-        }}
-        speed={2000}
-        slidesPerView={2}
-        spaceBetween={10}
-      >
-        {data.map((product) => (
-          <SwiperSlide key={product.id}>
-            <ProductItem data={product} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
+    if (!Array.isArray(data)) {
+        console.warn('data가 배열이 아닙니다:', data);
+        return <div>상품 정보를 불러오는 중입니다...</div>; // 로딩 또는 에러 UI 표시
+    }
+
+    return (
+        <div>
+            <Swiper
+                modules={[Autoplay, Pagination]}
+                pagination={{ clickable: true }}
+                navigation
+                loop
+                autoplay={{
+                    delay: 500,
+                    disableOnInteraction: false,
+                }}
+                breakpoints={{
+                    475: { slidesPerView: 3 },
+                    640: { slidesPerView: 4 },
+                    1024: { slidesPerView: 5 },
+                }}
+                speed={2000}
+                slidesPerView={2}
+                spaceBetween={10}
+            >
+                {data.map((product) => (
+                    <SwiperSlide key={product.id}>
+                        <ProductItem data={product} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
 };
 
 export default ProductListSwiper;
