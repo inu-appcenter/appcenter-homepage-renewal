d95ce92ec3cda1cb0ead11bd9dd8ebacdfa443d9
hjunieee <hinara12@inu.ac.kr>
2025-08-26 10:39:35 +0900

    fix: 앱 정보 모달의 height가 화면 높이를 넘는 문제

--- portal/src/components/home/product/ProductDetailModal.tsx
+++ portal/src/components/home/product/ProductDetailModal.tsx
@@ -22,38 +22,45 @@ const ProductDetailModal: React.FC<ProductDetailModalProps> = ({
   const { title, subTitle, body, images } = data;
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} size="xl" scrollBehavior="inside">
       <ModalOverlay />
-      <ModalContent className='relative mx-4' maxW='48rem' width='fit-content'>
+      <ModalContent
+        className="relative mx-4"
+        maxW="48rem"
+        maxH="90vh"   // 화면 높이 제한
+      >
         <ModalCloseButton />
-        <Image
-          className='absolute -top-0 -translate-y-1/2 left-4 rounded-2xl'
-          alt={`${title} 앱 아이콘`}
-          src={Object.values(images).at(0)}
-          width={100}
-        />
-        <div className='flex flex-col gap-y-8 px-4 py-8 pl-36'>
-          <div className='flex flex-col gap-y-6'>
-            <div>
-              <p className='font-semibold text-3xl'>{title}</p>
-              <p className='text-grayscale-600'>{subTitle}</p>
+        <div className="overflow-y-auto px-4 py-8 pl-36">
+          <Image
+            className="absolute -top-0 -translate-y-1/2 left-4 rounded-2xl"
+            alt={`${title} 앱 아이콘`}
+            src={Object.values(images).at(0)}
+            width={100}
+          />
+          <div className="flex flex-col gap-y-8">
+            <div className="flex flex-col gap-y-6">
+              <div>
+                <p className="font-semibold text-3xl">{title}</p>
+                <p className="text-grayscale-600">{subTitle}</p>
+              </div>
+              <p>{body}</p>
             </div>
-            <p>{body}</p>
-          </div>
-          <div className='flex gap-4 flex-wrap'>
-            {Object.values(images)
-              .slice(1)
-              .map((image) => (
-                <Image
-                  key={image}
-                  className='rounded-2xl w-full sm:w-44'
-                  src={image}
-                />
-              ))}
+            <div className="flex gap-4 flex-wrap">
+              {Object.values(images)
+                .slice(1)
+                .map((image) => (
+                  <Image
+                    key={image}
+                    className="rounded-2xl w-full sm:w-44"
+                    src={image}
+                  />
+                ))}
+            </div>
           </div>
         </div>
       </ModalContent>
     </Modal>
+
   );
 };
 
